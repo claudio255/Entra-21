1576ddcd2476eea18917fd0d15dd62ce3f3276a8
João Vitor Altermann Martinelli <joaomarti755@gmail.com>
2022-05-13 21:17:24 -0300

    Exercicio01

---
+++ ExercicioFOR/Exercicio01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFOR
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            for(int i = 0; i < 13; i++)
+            {
+                Console.WriteLine("Digite o nome da peça: ");
+                var nome = Console.ReadLine();
+                Console.WriteLine("Digite o valor da peça: ");
+                var valor = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ ExercicioFOR/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExercicioFOR;
+
+
+Console.WriteLine(@"----------------MENU------------
+1 - Exercicio01");
+var opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+if(opcaoEscolhida == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
