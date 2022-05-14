2166e4d9c811ee4fc8df4aa557a507040243fc56
João Vitor Altermann Martinelli <joaomarti755@gmail.com>
2022-05-13 21:46:29 -0300

    Exercicio08

---
+++ ExercicioFOR/Exercicio08.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFOR
+{
+    internal class Exercicio08
+    {
+        public void Executar()
+        {
+            Console.WriteLine("Digite qualquer numero inteiro: ");
+            var numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            for(int i = 0; i < 1000; i++)
+            {
+                i = i * numeroDigitado;
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
