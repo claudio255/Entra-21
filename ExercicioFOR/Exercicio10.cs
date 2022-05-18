4803b058f015d2c462b1f144a4ec74a237e3bcf7
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-18 19:03:01 -0300

    Exercicio10

---
+++ ExercicioFOR/Exercicio10.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFOR
+{
+    internal class Exercicio10
+    {
+        public void Executar()
+        {
+            var numeroString = true;
+            var numero = 0;
+            while (numeroString == true)
+            {
+                try
+                {
+                    Console.WriteLine("Digite um numero inteiro qualquer: ");
+                    numero = Convert.ToInt32(Console.ReadLine());
+                    numeroString = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("O numero digitado nao é valido!");
+                    numeroString = true;
+                }
+            }
+            var ordemDecrescente = numero;
+            Console.WriteLine(@"
+Os 50 numeros antecessores pares: 
+");
+            for (int i = 0; i < 50; i++)
+            {
+                ordemDecrescente = ordemDecrescente - 1;
+                if (ordemDecrescente % 2 == 0)
+                {
+                    Console.WriteLine(ordemDecrescente);
+
+                }
+            }
+            var ordemCrescente = numero;
+            Console.WriteLine(@"
+Os 50 numeros sucessores impares são:
+");
+            for(int i = 0; i < 50; i++)
+            {
+                ordemCrescente = ordemCrescente + 1;
+                if(ordemCrescente % 2 != 0)
+                {
+                    Console.WriteLine(ordemCrescente);
+                }
+            }
+        }
+    }
+}
