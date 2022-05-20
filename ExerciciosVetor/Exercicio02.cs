ffd2c85520b8e376ae875975b50a374c668486e3
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-20 18:53:47 -0300

    Exercicio02

--- ExerciciosVetor/Exercicio02.cs
+++ ExerciciosVetor/Exercicio02.cs
@@ -8,5 +8,28 @@ namespace ExerciciosVetor
 {
     internal class Exercicio02
     {
+        public void Executar()
+        {
+            string[] nomes = new string[10];
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Digite o nome que deseja: ");
+                nomes[i] = Console.ReadLine().Trim().ToLower();
+
+                while (nomes[i].Length < 3 || nomes[i].Length > 30)
+                {
+                    Console.WriteLine(@"O nome não pode ter menos que três caracteres ou mais que trinta!
+Por favor digite o nome novamente: ");
+                    nomes[i] = Console.ReadLine().Trim().ToLower();
+                }
+            }
+            Console.Clear();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+        }
     }
 }
--- ExerciciosVetor/Program.cs
+++ ExerciciosVetor/Program.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine(@"--------------MENU-------------
 1 - Exercicio 01
+2 - Exercicio 02
 Escolha a opção que deseja: ");
 var opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
 
@@ -9,5 +10,10 @@ if(opcaoEscolhida == 1)
 {
     Exercicio01 exercicio01 = new Exercicio01();
     exercicio01.Executar();
+}
+if(opcaoEscolhida == 2)
+{
+    Exercicio02 exercicio02 = new Exercicio02();
+    exercicio02.Executar();
 }
 
