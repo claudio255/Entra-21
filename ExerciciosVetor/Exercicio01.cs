a2e0eac8d23bf47e1cba261d1f7158e27b9b5b70
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-20 18:38:52 -0300

    Exercicio

---
+++ ExerciciosVetor/Exercicio01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosVetor
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            int[] numeros = new int[16];
+
+            for(int i = 0; i < 16; i++)
+            {
+                Console.WriteLine("Digite um numero inteiro: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+
+            int soma = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                soma = soma + numeros[i];
+            }
+            Console.WriteLine(@$"A soma dos numeros resulta em: {soma}");
+        }
+    }
+}
---
+++ ExerciciosVetor/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExerciciosVetor;
+
+Console.WriteLine(@"--------------MENU-------------
+1 - Exercicio 01
+Escolha a opção que deseja: ");
+var opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+if(opcaoEscolhida == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
+
