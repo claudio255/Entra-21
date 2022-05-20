b51022cbffa9fa7a63f495f8b4274dd9d6ea2cd8
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-20 19:38:15 -0300

    Program

---
+++ ExerciciosVetor/Exercicio04.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosVetor
+{
+    internal class Exercicio04
+    {
+        public void Executar()
+        {
+            double[] notas = new double[4];
+
+            Console.WriteLine("Digite o seu nome: ");
+            var nome = Console.ReadLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                try
+                {
+                    Console.WriteLine("Digite as suas notas: ");
+                    notas[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(@"Essa nota não pode ser aceita!
+Por favor digite novamente a nota: ");
+                    notas[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                if (notas[i] < 0 || notas[i] > 10)
+                {
+                    Console.WriteLine("A nota digitada é inferior a zero ou superior a dez!\nPor favor digite-a novamente: ");
+                    notas[i] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            double mediaNotas = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+            Console.WriteLine($@"A media do aluno {nome} é {mediaNotas}");
+        }
+    }
+}
--- ExerciciosVetor/Program.cs
+++ ExerciciosVetor/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine(@"--------------MENU-------------
 1 - Exercicio 01
 2 - Exercicio 02
 3 - Exercicio 03
+4 - Exercicio 04
 Escolha a opção que deseja: ");
 var opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
 
@@ -21,5 +22,10 @@ if(opcaoEscolhida == 3)
 {
     Exercicio03 exercicio03 = new Exercicio03();
     exercicio03.Executar();
+}
+if(opcaoEscolhida == 4)
+{
+    Exercicio04 exercicio04 = new Exercicio04();
+    exercicio04.Executar();
 }
 
