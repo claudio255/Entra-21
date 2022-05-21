5b240f111d424be2bb16a27f1c865dd55d9f08f8
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-20 21:54:41 -0300

    Exercicio07

--- ExerciciosVetor/Exercicio07.cs
+++ ExerciciosVetor/Exercicio07.cs
@@ -10,7 +10,37 @@ namespace ExerciciosVetor
     {
         public void Executar()
         {
+            double[] altura = new double[4];
 
+            var nomeMenorAnimal = ""; var menorAltura = double.MaxValue;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Digite o nome do animal: ");
+                var nomeAnimal = Console.ReadLine();
+
+                var alturaValida = false;
+                while (alturaValida == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("Digite a altura do animal: ");
+                        altura[i] = Convert.ToDouble(Console.ReadLine());
+
+                        alturaValida = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("A altura do animal não é valida!");
+                    }
+                }
+                if(altura[i] < menorAltura)
+                {
+                    menorAltura = altura[i];
+                    nomeMenorAnimal = nomeAnimal;
+                }
+            }
+            Console.WriteLine($"O animal com a menor altura é o(a) {nomeMenorAnimal} com uma altura de {menorAltura}");
         }
     }
 }
