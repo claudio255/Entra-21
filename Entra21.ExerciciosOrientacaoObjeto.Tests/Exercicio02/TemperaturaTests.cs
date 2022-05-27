0a31ba08149cbc66bbd1e5d89ef1c5c028d67c09
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-27 20:02:28 -0300

    Tests Temperatura

---
+++ Entra21.ExerciciosOrientacaoObjeto.Tests/Exercicio02/TemperaturaTests.cs
@@ -0,0 +1,29 @@
+﻿using Entra21.ExerciciosOrientacaoObjeto.Exercicio02;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Tests.Exercicio02
+{
+    public class TemperaturaTests
+    {
+        [Fact]
+        public void Validar_CalcularCelsius_Kelvin_Para_Celsius()
+        {
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaDeOrigem = "kelvin";
+            temperatura.TemperaturaDeDestino = "celsius";
+            temperatura.TemperaturaValor = 300;
+            
+
+            var calcularKelvinParaCelsius = temperatura.CalcularCelsius();
+
+            var resultadoCelsius = temperatura.TemperaturaValor - 273.15;
+            calcularKelvinParaCelsius.Should().Be(resultadoCelsius);
+        }
+    }
+}
