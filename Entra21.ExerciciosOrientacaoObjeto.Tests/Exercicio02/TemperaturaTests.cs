fcd46c1c572a612041e744452a7c12ca86c8a91b
claudio255 <claudio.h.raimundo123@gmail.com>
2022-05-27 21:20:17 -0300

    Tests Temperatura

--- Entra21.ExerciciosOrientacaoObjeto.Tests/Exercicio02/TemperaturaTests.cs
+++ Entra21.ExerciciosOrientacaoObjeto.Tests/Exercicio02/TemperaturaTests.cs
@@ -22,8 +22,78 @@ namespace Entra21.ExerciciosOrientacaoObjeto.Tests.Exercicio02
 
             var calcularKelvinParaCelsius = temperatura.CalcularKelvinParaCelsius();
 
-            var resultadoCelsius = temperatura.TemperaturaValor - 273.15;
-            calcularKelvinParaCelsius.Should().Be(resultadoCelsius);
+            var conversaoParaCelsius = temperatura.TemperaturaValor - 273.15;
+            calcularKelvinParaCelsius.Should().Be(conversaoParaCelsius);
+        }
+
+        [Fact]
+        public void Validar_CalcularCelsius_Fahrenheit_Para_Celsius()
+        {
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaDeOrigem = "fahrenheit";
+            temperatura.TemperaturaDeDestino = "celsius";
+            temperatura.TemperaturaValor = 300;
+
+            var calcularFahrenheitParaCelsius = temperatura.CalcularFahrenheitParaCelsius();
+
+            var conversaoParaCelsius = (temperatura.TemperaturaValor - 32) * 1.8;
+            calcularFahrenheitParaCelsius.Should().Be(conversaoParaCelsius);
+        }
+
+        [Fact]
+        public void Validar_CalcularKelvin_Celsius_Para_Kelvin()
+        {
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaDeOrigem = "celsius";
+            temperatura.TemperaturaDeDestino = "kelvin";
+            temperatura.TemperaturaValor = 300;
+
+            var calcularCelsiusParaKelvin = temperatura.CalcularCelsiusParaKelvin();
+
+            var conversaoParaKelvin = temperatura.TemperaturaValor + 273.15;
+            calcularCelsiusParaKelvin.Should().Be(conversaoParaKelvin);
+        }
+
+        [Fact]
+        public void Validar_CalcularKelvin_Fahrenheit_Para_Kelvin()
+        {
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaDeOrigem = "fahrenheit";
+            temperatura.TemperaturaDeDestino = "kelvin";
+            temperatura.TemperaturaValor = 300;
+
+            var calcularFahrenheitParaKelvin = temperatura.CalcularFahrenheitParaKelvin();
+
+            var conversaoParaKelvin = (temperatura.TemperaturaValor - 32) * 5 / 9 + 273.15;
+            calcularFahrenheitParaKelvin.Should().Be(conversaoParaKelvin);
+        }
+
+        [Fact]
+        public void Validar_CalcularFahrenheit_Celsius_Para_Fahrenheit()
+        {
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaDeOrigem = "celsius";
+            temperatura.TemperaturaDeDestino = "fahrenheit";
+            temperatura.TemperaturaValor = 300;
+
+            var calcularCelsiusParaFahrenheit = temperatura.CalcularCelsiusParaFahrenheit();
+
+            var conversaoParaFahrenheit = (temperatura.TemperaturaValor * 1.8) + 32;
+            calcularCelsiusParaFahrenheit.Should().Be(conversaoParaFahrenheit);
+        }
+
+        [Fact]
+        public void Validar_CalcularFahrenheit_Kelvin_Para_Fahrenheit()
+        {
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaDeOrigem = "kelvin";
+            temperatura.TemperaturaDeDestino = "fahrenheit";
+            temperatura.TemperaturaValor = 300;
+
+            var calcularKelvinParaFahrenheit = temperatura.CalcularKelvinParaFahrenheit();
+
+            var conversaoParaFahrenheit = (temperatura.TemperaturaValor - 273.15) * 9 / 5 + 32;
+            calcularKelvinParaFahrenheit.Should().Be(conversaoParaFahrenheit);
         }
     }
 }
