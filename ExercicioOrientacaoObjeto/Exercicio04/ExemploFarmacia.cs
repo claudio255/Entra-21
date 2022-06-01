8554f7e56c8885549a282825cf4292ff464dc7e8
claudio255 <claudio.h.raimundo123@gmail.com>
2022-06-01 18:45:23 -0300

    Exercicio Farmacia

--- ExercicioOrientacaoObjeto/Exercicio04/ExemploFarmacia.cs
+++ ExercicioOrientacaoObjeto/Exercicio04/ExemploFarmacia.cs
@@ -17,14 +17,14 @@ namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio04
             farmacia.NomeProduto2 = "ibuprofeno";
             farmacia.CategoriaProduto2 = "antibiotico";
             farmacia.PrecoProduto2 = 22.22;
-            farmacia.NomeProduto3 = "remedia";
-            farmacia.CategoriaProduto3 = "antibiotico";
+            farmacia.NomeProduto3 = "remedio";
+            farmacia.CategoriaProduto3 = "antiflamatorio";
             farmacia.PrecoProduto3 = 10.03;
 
             Console.WriteLine(@$"O valor total do pedido é {farmacia.ApresentarTotalDoPedido()}
-O preço total da categoria 1 é: {farmacia.ApresentarTotalPedidoCategoriaAntiflamatorio()}
-O preço total da categoria 2 é: {farmacia.ApresentarPorCategoriaAntibiotico()}
-O preço total da categoria 3 é: {farmacia.ApresentarPorCategoriaAntidepressivo()}
+O preço total dos antidepressivos é: {farmacia.ApresentarTotalPedidoCategoriaAntiflamatorio()}
+O preço total dos antibioticos é: {farmacia.ApresentarPorCategoriaAntibiotico()}
+O preço total dos antiflamatorios é: {farmacia.ApresentarPorCategoriaAntidepressivo()}
 O produto mais barato é: {farmacia.ApresentarNomeCategoriaDoProdutoMaisBarato()}
 O produto mais caro é: {farmacia.ApresentarNomeProdutoMaisCaro()}");
         }
