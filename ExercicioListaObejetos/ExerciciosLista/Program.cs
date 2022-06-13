302510e4688c748df32c6cbb6fe32d79ab42494b
claudio255 <claudio.h.raimundo123@gmail.com>
2022-06-13 19:34:18 -0300

    Exercicio Aluno Lista Program Finalizada

--- ExercicioListaObejetos/ExerciciosLista/Program.cs
+++ ExercicioListaObejetos/ExerciciosLista/Program.cs
@@ -1,4 +1,18 @@
 ﻿using ExercicioListaObejetos;
+using ExercicioListaObejetos.Exercicio02AlunoLista;
 
-TrianguloControlador controlador = new TrianguloControlador();
-controlador.GerenciarMenu();
+Console.WriteLine(@"-----------MENU---------
+1 - Exercicio Triangulo
+2 - Exercicio Aluno");
+var opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoEscolhida == 1)
+{
+    TrianguloControlador triangulo = new TrianguloControlador();
+    triangulo.GerenciarMenu();
+}
+else if (opcaoEscolhida == 2)
+{
+    AlunoControler aluno = new AlunoControler();
+    aluno.GerenciarMenu();
+}
