cb840c53747bbb9c9669b51acabdd8559597b581
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-06-15 09:31:01 -0300

    Tentativa program

--- ExemploProdutoLista/ProdutoControlador.cs
+++ ExemploProdutoLista/ProdutoControlador.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExemploProdutoLista
+﻿namespace ExemploProdutoLista
 {
     internal class ProdutoControlador
     {
--- ExemploProdutoLista/Program.cs
+++ ExemploProdutoLista/Program.cs
@@ -1,7 +1,18 @@
 ﻿using ExemploProdutoLista;
 
+Console.WriteLine(@"----------MENU------------
+1 - Produto
+2 - Aluno");
+var opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+if(opcaoEscolhida == 1)
+{
 ProdutoControlador controlador = new ProdutoControlador();
 controlador.GerenciarMenu();
+}
 
-//AlunoControler aluno = new AlunoControler();
-//aluno.GerenciarMenu();
+//if(opcaoEscolhida == 2)
+//{
+//    AlunoControler aluno = new AlunoControler();
+//    aluno.GerenciarMenu();
+//}
