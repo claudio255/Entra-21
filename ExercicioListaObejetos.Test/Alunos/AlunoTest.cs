5738ec5513cde85495d04f9deaa532d37061069a
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-06-16 10:47:16 -0300

    tentativa test 2

--- ExercicioListaObejetos.Test/Alunos/AlunoTest.cs
+++ ExercicioListaObejetos.Test/Alunos/AlunoTest.cs
@@ -20,5 +20,27 @@ namespace ExercicioListaObejetos.Test.Alunos
 
             apagarAluno.Should().Be(true);
         }
+
+        [Fact]
+        public void Validar_AdicionarAluno()
+        {
+            var aluno = new Aluno();
+            var alunoServico = new AlunoServico();
+            
+            aluno.Nome = "claudio";
+            aluno.Idade = 22;
+            aluno.MateriaFavorita = "fisica";
+            aluno.Nota1 = 8.3;
+            aluno.Nota2 = 9;
+            aluno.Nota3 = 7.4;
+            aluno.Status = AlunoStatus.Aprovado;
+            
+            alunoServico.AdicionarAluno("claudio", 22, "fisica", 8.3, 9, 7.4);
+
+            var addAluno = alunoServico.AdicionarAluno(aluno.Nome, aluno.Idade, aluno.MateriaFavorita, aluno.Nota1, aluno.Nota2, aluno.Nota3);
+
+            addAluno.Should().Be(true);
+        }
+
     }
 }
