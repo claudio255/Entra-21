838afc22eef0b0a985f46b0ef8a2dcb9195a1057
claudio255 <claudio.h.raimundo123@gmail.com>
2022-06-20 18:14:16 -0300

    .

--- Entra21.ExemplosWindowsForms/exemplo02/MediaForms.cs
+++ Entra21.ExemplosWindowsForms/exemplo02/MediaForms.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Entra21.ExemplosWindowsForms.exemplo02
+﻿namespace Entra21.ExemplosWindowsForms.exemplo02
 {
     public partial class MediaForms : Form
     {
