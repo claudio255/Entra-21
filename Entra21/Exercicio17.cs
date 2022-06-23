6984d34a43a00d797b9e2f9f39bb99b0b9d2ea08
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-06-23 12:01:40 -0300

    Exercicio IF

--- Entra21/Exercicio17.cs
+++ Entra21/Exercicio17.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entra21
+﻿namespace Entra21
 {
     internal class Exercicio17
     {
+        public void Executar()
+        {
+            //            17.Solicite o ano de nascimento e apresente o ano escrito por extenso.
+            //Exemplo: 1993 deverá apresentar para o usuário “Mil novecentos e noventa e três”
+            Console.WriteLine("Digite o seu ano de nascimento: ");
+            var nascimento = Convert.ToInt32(Console.ReadLine());
+
+            if (nascimento == )
+            {
+                var milhar = nascimento - 1000;
+                Console.WriteLine("mil");
+            }
+        }
     }
 }
